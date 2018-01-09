225725f3048bc5fd175ead41f438abba016b5013
Mark Wales <mark@smallhadroncollider.com>
2018-01-09 18:47:32 +0000

    updated Formula with bottles

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -4,16 +4,14 @@ class Taskell < Formula
   url "https://github.com/smallhadroncollider/taskell/archive/0.8.1.tar.gz"
   sha256 "31d89252341af7f34affba2421bf8ed471472e29aa33e57d18dd164d7b214fc3"
 
-  depends_on "ghc" => :build
-  depends_on "cabal-install" => :build
   depends_on "stack" => :build
 
-  # bottle do
-  #   root_url "https://files.smallhadroncollider.com/taskell"
-  #   cellar :any_skip_relocation
-  #   sha256 "4188ffd54bf115d83736e62dee7ba32ee9f58321e87cf8ae91a4f6487980a838" => :sierra
-  #   sha256 "6403ef1a746ae554f063094c820bfffd27f9a7952469643df78842f88d2ff011" => :high_sierra
-  # end
+  bottle do
+    root_url "https://files.smallhadroncollider.com/taskell"
+    cellar :any_skip_relocation
+    sha256 "2c7a10ebcda4b3dcbae7dd8eda40ea611b5ad45bfabb6c017573723098b25413" => :sierra
+    sha256 "80013d21461fd2dc80555ff5fd3a5f8b152078b88e7ea3fd1e697eac187ce879" => :high_sierra
+  end
 
   def install
     system "stack", "build"
