708d6406ecc7475417c60e8d148e7ad9d6ef63d0
Mark Wales <mark@smallhadroncollider.com>
2018-01-10 19:45:02 +0000

    added high sierra bottle for 0.9.0

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -10,7 +10,7 @@ class Taskell < Formula
     root_url "https://files.smallhadroncollider.com/taskell"
     cellar :any_skip_relocation
     sha256 "e567deeb6f6ffe8437ffd3645d9080b112f04536678dccdee650ec70a71c116a" => :sierra
-    # sha256 "80013d21461fd2dc80555ff5fd3a5f8b152078b88e7ea3fd1e697eac187ce879" => :high_sierra
+    sha256 "fd6c77d1f3ce7b935a9392dee4364cfcf452d11586746d6f6354332d9466e4fe" => :high_sierra
   end
 
   def install
