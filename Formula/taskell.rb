2d3ebd0c95ddaf2209439c0d3a3a728af8f005d5
Mark Wales <mark@smallhadroncollider.com>
2018-01-12 09:30:59 +0000

    version bump to 0.9.2

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -1,15 +1,14 @@
 class Taskell < Formula
   desc "A command line task manager written in Haskell"
   homepage "https://github.com/smallhadroncollider/taskell"
-  url "https://github.com/smallhadroncollider/taskell/archive/0.9.1.tar.gz"
-  sha256 "a3d1e0affec6abb3d953136c390387d79259585645474dca3627d61a9d2f5787"
+  url "https://github.com/smallhadroncollider/taskell/archive/0.9.2.tar.gz"
+  sha256 "ace2c518763d5b235b7c18b6e616bc103a935b3fdc51be01ecefe67c2dce81d0"
 
   depends_on "stack" => :build
 
   bottle do
     root_url "https://files.smallhadroncollider.com/taskell"
     cellar :any_skip_relocation
-    sha256 "71553e7e7cb5938314b245f35b13ab5d84ac7d4e35fdd164b7539a27f6ab0175" => :high_sierra
   end
 
   def install
