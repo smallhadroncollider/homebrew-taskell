0cd0afe97841183beb0b2073f8b178da70e57c71
Mark Wales <mark@smallhadroncollider.com>
2018-05-10 12:05:37 +0100

    updated formula to match rubocop's advice

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -1,10 +1,15 @@
 class Taskell < Formula
-  desc "A command line kanban board/task manager written in Haskell"
-  homepage "https://github.com/smallhadroncollider/taskell"
+  desc "Command-line kanban board/task manager written in Haskell"
+  homepage "https://twitter.com/taskellcli"
   url "https://github.com/smallhadroncollider/taskell/archive/1.1.0.tar.gz"
   sha256 "77ad4aaeb1121445f2f6f7553d75e0c1fa47734ac3089c4595b8ba1d27b9b126"
 
-  depends_on "stack" => :build
+  depends_on "haskell-stack" => :build
+
+  def install
+    system "stack", "build"
+    system "stack", "install", "--local-bin-path=#{bin}"
+  end
 
   bottle do
     root_url "https://files.smallhadroncollider.com/taskell"
@@ -13,8 +18,7 @@ class Taskell < Formula
     sha256 "ae70c2ed208ae3d2143e628b23fe886a3801d029716ee51f38e19ea1455db2bf" => :sierra
   end
 
-  def install
-    system "stack", "build"
-    system "stack", "install", "--local-bin-path=#{bin}"
+  test do
+    system "#{bin}/taskell", "-v"
   end
 end
