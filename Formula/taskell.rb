797771365268b9d6ef5b74bbc75d462f8677f638
Mark Wales <mark@smallhadroncollider.com>
2018-05-18 09:51:52 +0100

    swapped to cabal install

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -1,20 +1,25 @@
+require "language/haskell"
+
 class Taskell < Formula
+  include Language::Haskell::Cabal
+
   desc "Command-line kanban board/task manager written in Haskell"
   homepage "https://taskell.app"
-  url "https://github.com/smallhadroncollider/taskell/archive/1.2.0.tar.gz"
-  sha256 "cdde3d14c32332fb8002e67a6f9b6b92fd2878d6e1c23d05e16466bdaee53e90"
+  url "https://github.com/smallhadroncollider/taskell/archive/1.2.1.tar.gz"
+  sha256 "6ace0f7465f896f7f8421263458360823354a32e120b6876e6ba4511ffb31ed4"
 
-  depends_on "haskell-stack" => :build
+  depends_on "ghc@8.2" => :build
+  depends_on "cabal-install" => :build
 
   def install
-    system "stack", "build"
-    system "stack", "install", "--local-bin-path=#{bin}"
+    cabal_install "hpack"
+    system "./.cabal-sandbox/bin/hpack"
+    install_cabal_package
   end
 
   bottle do
     root_url "https://taskell.app/bottles"
     cellar :any_skip_relocation
-    sha256 "3164d83a46d8f72879bcfb942ae2d67cb3510819c911d7338e913045ade257af" => :high_sierra
   end
 
   test do
