e4641d7d5eda4d4bd1692a81f219789249cfa813
Mark Wales <mark@smallhadroncollider.com>
2018-05-21 16:50:17 +0100

    version bump

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -1,8 +1,8 @@
 class Taskell < Formula
   desc "Command-line kanban board/task manager written in Haskell"
   homepage "https://twitter.com/taskellcli"
-  url "https://github.com/smallhadroncollider/taskell/archive/1.1.0.tar.gz"
-  sha256 "77ad4aaeb1121445f2f6f7553d75e0c1fa47734ac3089c4595b8ba1d27b9b126"
+  url "https://github.com/smallhadroncollider/taskell/archive/1.2.0.tar.gz"
+  sha256 "cdde3d14c32332fb8002e67a6f9b6b92fd2878d6e1c23d05e16466bdaee53e90"
 
   depends_on "haskell-stack" => :build
 
@@ -14,8 +14,6 @@ class Taskell < Formula
   bottle do
     root_url "https://files.smallhadroncollider.com/taskell"
     cellar :any_skip_relocation
-    sha256 "23bb58449d4ea724a6cc47a1f95b8005c834c219fa0263de437b89008fe1978d" => :high_sierra
-    sha256 "ae70c2ed208ae3d2143e628b23fe886a3801d029716ee51f38e19ea1455db2bf" => :sierra
   end
 
   test do
