4974ae06a5a64a66367c8098a074052b78015d3a
Mark Wales <mark@smallhadroncollider.com>
2018-05-24 11:13:49 +0100

    trying with cabal-install again

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -1,20 +1,27 @@
-class Taskell < Formula
-  desc "Command-line kanban board/task manager written in Haskell"
-  homepage "https://taskell.app"
-  url "https://github.com/smallhadroncollider/taskell/archive/1.2.0.tar.gz"
-  sha256 "cdde3d14c32332fb8002e67a6f9b6b92fd2878d6e1c23d05e16466bdaee53e90"
+require "language/haskell"
 
-  depends_on "haskell-stack" => :build
+class Taskell < Formula
+  include Language::Haskell::Cabal
+
+  desc "Command-line Kanban board/task manager with support for Trello"
+  homepage "https://taskell.app"
+  url "https://github.com/smallhadroncollider/taskell/archive/1.2.4.tar.gz"
+  sha256 "88e060bd6ae4a9f421b762263562bd1b59b408c0ef3b158f2a83970207095052"
+
+  depends_on "ghc" => :build
+  depends_on "cabal-install" => :build
 
   def install
-    system "stack", "build"
-    system "stack", "install", "--local-bin-path=#{bin}"
+    cabal_sandbox do
+      cabal_install "hpack"
+      system "./.cabal-sandbox/bin/hpack"
+      install_cabal_package "-f", "release"
+    end
   end
 
   bottle do
     root_url "https://taskell.app/bottles"
     cellar :any_skip_relocation
-    sha256 "3164d83a46d8f72879bcfb942ae2d67cb3510819c911d7338e913045ade257af" => :high_sierra
   end
 
   test do
